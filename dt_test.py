92c3c4741c063edad6b637d7bd36d60ad1ee495e
Przemek Pilarski <prz.pilarski@gmail.com>
2022-12-20 17:48:59 +0100

    delaunay triangulation

--- dt_test.py
+++ dt_test.py
@@ -5,10 +5,29 @@ import numpy as np
 from shapely import affinity
 import math
 import time
-#shape
-polygon = Polygon([[0.0, 1], [5,1], [40, 30], [30, 42], [20, 47], [10, 50], 
-                [0, 51], [-10, 50], [-20, 47], [-30, 42], [-40, 30], [-5,1]])
+from scipy.spatial import Delaunay
+from iter_through_points import*
 
+'''
+Checking if the triangle is locked:
+    i can go only to the other vertex of the next triangle
+        get possible points to go to                        findTriangles
+        get distances to all possible points                findTriangles
+        go to the closest point and save rest in memory
+        mark point as taken
+    other side
+        get possible points to go to
+        if both points are taken go to previous side and change point to the second closest
+
+        get possible points to go to
+        get distances to all possible points
+        go to the closest point and save rest in memory
+        mark point as taken
+'''
+
+#numpy_points = np.array([[-239.28590431227303, 121.98321404586304], [-190.71542073733096, 140.63352377547224], [-138.33644801159795, 137.91706071832806], [-88.71535492871936, 160.60442135514262], [-35.925593893357494, 151.4802087335676], [-5.4383184555142705, 106.75686308185043], [12.692113868883062, 55.09782223305757], [14.99999930125987, 0.004578449893607691], [-14.99999930125987, -0.004578449893606847], [-18.691676190262022, 62.082153394862786], [-44.267023422459474, 118.67472829694223], [-101.2771911667341, 125.92273809878164], [-157.15955484391569, 101.58456411729983], [-215.39198132989281, 103.84263950942452]])
+#numpy_points = np.array([[ 31.,    5. ], [ 22.3,  17. ], [ 14.7,   3. ], [-15. ,  -3. ], [-15. ,   8. ], [-13. ,  19. ], [ -8. ,  28. ], [ -0. ,  37. ], [  9. ,  43. ], [ 19. ,  47. ], [ 30. ,  47. ], [ 40. ,  42. ], [ 48. ,  35. ], [ 54. ,  26. ], [ 59. ,  16. ]])
+#numpy_points = np.array([[-239.28590431227303, 121.98321404586304], [-190.71542073733096, 140.63352377547224], [-138.33644801159795, 137.91706071832806], [-88.71535492871936, 160.60442135514262]])
 numpy_points = np.array([[-136.3,  256. ],
  [-125.8,  255. ],
  [-115.5,  253. ],
@@ -72,91 +91,67 @@ numpy_points = np.array([[-136.3,  256. ],
  [-127. ,  225. ],
  [-138. ,  226. ]])
 
-start_point = Point(0,0)
+plt.scatter(*zip(*numpy_points), color='Black')
+plt.scatter(numpy_points[1][0],numpy_points[1][1],color='Red')
+plt.show()
 
+tri = Delaunay(numpy_points)
+triangle = numpy_points[tri.simplices]
 
+point = numpy_points[1]
+exclude_point = numpy_points[0]
 
-def points_inside(polygon, points):
-    mid = Point(0,0)
-    points_inside_polygon = []
+def findTriangles(triangulation, point, exclude_point, max_side_length):
+    triangles = []
     distances = []
-    x,y = polygon.exterior.xy
-    xs, ys = extract_points_shapely(points)
-    for i, val in enumerate(points):
-        if polygon.contains(points[i]) is True:
-            #print(mid.distance(points[i]), points[i])
-            points_inside_polygon.append(points[i])
-            distances.append(mid.distance(points[i]))
+    points = []
+    for triangle in triangulation.simplices:
+        if point in numpy_points[triangle] and exclude_point not in numpy_points[triangle]:
 
-    if distances == []:
-        print('finished')
-        closest_point = 0
-        return points_inside_polygon, closest_point
-    closest = distances.index(min(distances))
-    print('point', closest)
-    print(points_inside_polygon[closest])
-    closest_point = points_inside_polygon[closest]
+            sides = [(numpy_points[triangle[0]], numpy_points[triangle[1]]),
+                     (numpy_points[triangle[1]], numpy_points[triangle[2]]),
+                     (numpy_points[triangle[2]], numpy_points[triangle[0]])]
+            skip_triangle = False
+            for side in sides:
+                distance = np.linalg.norm(side[0] - side[1])
+                if distance > max_side_length:
+                    skip_triangle = True
+                    break
+            if skip_triangle:
+                continue
+            triangles.append(triangle)
+            for p in numpy_points[triangle]:
+                distance = np.linalg.norm(point - p)
+                distances.append(distance)
+                points.append(p)
+    return triangles, distances, points
 
-    return points_inside_polygon, closest_point
+triangles, distances, points = findTriangles(tri, point, exclude_point, 100)
 
-def extract_points_shapely(point_list):
-    xs = [point.x for point in point_list]
-    ys = [point.y for point in point_list]
-    return xs, ys
+print(triangles)
+print(distances)
+print(points)
 
-def rotate_poly(poly, radians, rotate_around):
-    r_poly = affinity.rotate(poly, radians, use_radians=True, origin=rotate_around)
-    return r_poly
+for triangle in triangles:
+    x = [numpy_points[i][0] for i in triangle]
+    y = [numpy_points[i][1] for i in triangle]
+    plt.fill(x, y, color='red')
 
-def translate_poly(poly, vector):
-    trans_x, trans_y = vector
-    t_poly = affinity.translate(polygon, trans_x, trans_y)
-    return t_poly
+plt.triplot(numpy_points[:,0], numpy_points[:,1], tri.simplices)
+plt.plot(numpy_points[:,0], numpy_points[:,1], 'o')
+plt.show()
 
-def nparray_to_pointarray(points):
-    point_arr = []
-    for i, val in enumerate(points):
-        point_arr.append(Point((points[i][0]), (points[i][1])))
-    return point_arr
-
-def calculate_angle(points):
-    p1 = points[-2]
-    p2 = points[-1]
-    p3 = points[-3]
-    
-    p12 = math.sqrt((points[-2][0]-points[-1][0])**2 + (points[-2][1]-points[-1][1])**2)
-    p23 = math.sqrt((points[-1][0]-points[-3][0])**2 + (points[-1][1]-points[-3][1])**2)
-    p13 = math.sqrt((points[-2][0]-points[-3][0])**2 + (points[-2][1]-points[-3][1])**2)
-
-
-    radians = np.arccos((p12**2 + p13**2 - p23**2)/(2 * p12 * p13))
-   
-    print(p12, p13, p23)
-    radians = math.pi - radians
-    angle = radians*(180/math.pi)
-    #print(angle)
-    #checking angle side
-    if ((points[-2][0] - points[-1][0])*(points[-3][1] - points[-1][1]) - (points[-2][1] - points[-1][1])*(points[-3][0] - points[-1][0])) > 0:
-        radians = -radians
-    return radians
-
-def remove_point(arr1, point_to_remove):
-    arr2 = np.array([[point_to_remove[0], point_to_remove[1]]])
-    delta = set(map(tuple, arr2))
-    return np.fromiter((x for xs in arr1 if tuple(xs) not in delta for x in xs), dtype=arr1.dtype).reshape(-1, arr1.shape[-1])
-
-
-#test points
-points = nparray_to_pointarray(numpy_points)
-# ex_points = extract_points_shapely(points_inside(polygon, points))
-# pi = points_inside(polygon, points)
+# iteration part
+polygon = Polygon([[0.0, 1], [5,1], [40, 30], [30, 42], [20, 47], [10, 50],
+                [0, 51], [-10, 50], [-20, 47], [-30, 42], [-40, 30], [-5,1]])
+start_point = Point(0,0)
 
 # start sequence:  get the first cone on the right and rotate 90 degrees to get the second point, after that we can get angle between 3 points (one being imaginary, right behind the first cone) and go on from there
 
 
     # right
 
-'''
+
 right_cones = [(15,-1)]
 right_vector = (0,0)
 right_deg90 = -3.14/2
@@ -168,7 +163,7 @@ right_ex_points = extract_points_shapely(right_points_in)
 right_closest_point = (right_closest.x, right_closest.y)  #next point to go to
 right_cones.append(right_closest_point)
 
-print(right_closest_point,' right') 
+print(right_closest_point,' right')
 
 plt.scatter(right_ex_points[0],right_ex_points[1])
 plt.plot(right_shape.exterior.xy[0], right_shape.exterior.xy[1])
@@ -182,15 +177,14 @@ right_ex_points = extract_points_shapely(right_points_in)
 right_closest_point = (right_closest.x, right_closest.y)  #next point to go to
 right_cones.append(right_closest_point)
 
-print(right_closest_point, ' right') 
+print(right_closest_point, ' right')
 print(right_cones)
 print(calculate_angle(right_cones))
-
-
 
 plt.scatter(right_ex_points[0],right_ex_points[1])
 plt.plot(second_right_shape.exterior.xy[0], second_right_shape.exterior.xy[1])
 plt.show()
+#####################################################################################
 
     # left
 
@@ -206,7 +200,7 @@ left_ex_points = extract_points_shapely(left_points_in)
 left_closest_point = (left_closest.x, left_closest.y)  #next point to go to
 left_cones.append(left_closest_point)
 
-print(left_closest_point, ' left') 
+print(left_closest_point, ' left')
 
 plt.scatter(left_ex_points[0],left_ex_points[1], edgecolors='Yellow')
 plt.plot(left_shape.exterior.xy[0], left_shape.exterior.xy[1])
@@ -220,7 +214,7 @@ left_ex_points = extract_points_shapely(left_points_in)
 left_closest_point = (left_closest.x, left_closest.y)  #next point to go to
 left_cones.append(left_closest_point)
 
-print(left_closest_point, ' left') 
+print(left_closest_point, ' left')
 print(left_cones)
 print(calculate_angle(left_cones))
 
@@ -229,6 +223,7 @@ plt.scatter(left_ex_points[0], left_ex_points[1], edgecolors='Yellow')
 plt.plot(second_left_shape.exterior.xy[0], second_left_shape.exterior.xy[1])
 plt.show()
 
+####################################################################################
 
 while 1:
     right_rads = calculate_angle(right_cones)
@@ -237,18 +232,17 @@ while 1:
     third_right_shape = translate_poly(second_right_shape, right_vector)
     third_right_shape = rotate_poly(third_right_shape, right_rads, right_vector)
     right_points_in, right_closest = points_inside(third_right_shape, points)
-    if right_closest  == 0:
-        break   # powinno byc go to left
+    if right_closest == 0:
+        break  # powinno byc go to left
     right_ex_points = extract_points_shapely(right_points_in)
-    right_closest_point = (right_closest.x, right_closest.y)  #next point to go to
+    right_closest_point = (right_closest.x, right_closest.y)  # next point to go to
     right_cones.append(right_closest_point)
-    #points = remove_point(points, right_closest_point)
-    print(right_cones,' right')
-    
+    # points = remove_point(points, right_closest_point)
+    print(right_cones, ' right')
+
     # plt.scatter(right_ex_points[0], right_ex_points[1])
     # plt.plot(third_right_shape.exterior.xy[0], third_right_shape.exterior.xy[1])
     # plt.show()
-
 
     left_rads = calculate_angle(left_cones)
     left_vector = left_closest_point
@@ -256,12 +250,12 @@ while 1:
     third_left_shape = translate_poly(second_left_shape, left_vector)
     third_left_shape = rotate_poly(third_left_shape, left_rads, left_vector)
     left_points_in, left_closest = points_inside(third_left_shape, points)
-    if left_closest  == 0:
+    if left_closest == 0:
         break
     left_ex_points = extract_points_shapely(left_points_in)
-    left_closest_point = (left_closest.x, left_closest.y)  #next point to go to
+    left_closest_point = (left_closest.x, left_closest.y)  # next point to go to
     left_cones.append(left_closest_point)
-    #points = remove_point(points, left_closest_point)
+    # points = remove_point(points, left_closest_point)
     print(left_cones, ' left')
 
     # plt.scatter(left_ex_points[0],left_ex_points[1], edgecolors='Yellow')
@@ -275,4 +269,3 @@ plt.scatter(*zip(*right_cones))
 
 plt.scatter(*zip(*left_cones), edgecolors='Yellow')
 plt.show()
-'''
